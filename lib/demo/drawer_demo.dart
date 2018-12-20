ed73adc901e0f57c2792b3ddded2542468b5f47d
capricorntb <yintb@aliyun.com>
2018-12-21 01:06:51 +0800

    界面结构

---
+++ lib/demo/drawer_demo.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class DrawerDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          UserAccountsDrawerHeader(
+            accountName: Text(
+              'tianbao',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            accountEmail: Text('nobody@nowhere'),
+          ),
+          ListTile(
+            title: Text(
+              'Messages',
+              textAlign: TextAlign.center,
+            ),
+            trailing: Icon(Icons.message, color: Colors.black12, size: 22),
+            onTap: () => Navigator.pop(context),
+          ),
+          ListTile(
+            title: Text(
+              'Favorite',
+              textAlign: TextAlign.center,
+            ),
+            trailing: Icon(Icons.favorite, color: Colors.black12, size: 22),
+            onTap: () => Navigator.pop(context),
+          ),
+          ListTile(
+            title: Text(
+              'settings',
+              textAlign: TextAlign.center,
+            ),
+            trailing: Icon(Icons.settings, color: Colors.black12, size: 22),
+            onTap: () => Navigator.pop(context),
+          ),
+        ],
+      ),
+    );
+  }
+}
